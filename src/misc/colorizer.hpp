bc4e573b04ddd5c52e99c06181a8ea4ddd25b38c
maksim-ruletov <maksim.manezkiy.evg.99@gmail.com>
2025-09-15 14:02:49 +0300

    0.00 - predev
    - Style fix

--- src/misc/colorizer.hpp
+++ src/misc/colorizer.hpp
@@ -5,11 +5,13 @@
 #include <string>
 
 namespace misc {
-    std::string colorizeFont (const std::string content, const ::size_t r, const ::size_t g, const ::size_t b) {
+    std::string colorizeFont (const std::string content, const ::size_t r, const ::size_t g, const ::size_t b)
+    {
         return "\033[38;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + content + "\033[0m";
     }
 
-    std::string colorizeBackground (const std::string content, const ::size_t r, const ::size_t g, const ::size_t b) {
+    std::string colorizeBackground (const std::string content, const ::size_t r, const ::size_t g, const ::size_t b)
+    {
         return "\033[48;2;" + std::to_string(r) + ";" + std::to_string(g) + ";" + std::to_string(b) + "m" + content + "\033[0m";
     }
 }
